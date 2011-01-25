7a45bab0ad6c2414d687e3e99a6e795e9785ef25
ceh <c.emil.hessman@gmail.com>
2011-01-25 09:16:00 +0100

    Use p to output characters as strings.

--- main.erl
+++ main.erl
@@ -10,11 +10,11 @@ start(FileName) ->
 process_file(FileDescriptor) ->
     case io:request(FileDescriptor, {get_until, prompt, lexer, token, [1]}) of
         {ok, {Type, _, Value}, _} ->
-            io:format('~w~n', [{Type, Value}]),
+            io:format('~p~n', [{Type, Value}]),
             process_file(FileDescriptor);
         {eof, _} ->
             ok;
         ErrorInfo ->
-            io:format('~w~n', [{lexical_error, ErrorInfo}]),
+            io:format('~p~n', [{lexical_error, ErrorInfo}]),
             erlang:halt()
     end.
