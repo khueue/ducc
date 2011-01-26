11c84b2eb68d5422ea15eb99f584cb00e5630fe3
khueue <khueue@gmail.com>
2011-01-26 17:32:54 +0100

    Cleanup.

--- README.md
+++ README.md
@@ -4,6 +4,4 @@ Rationale: <http://www.youtube.com/watch?v=FJ_VAh-V0EE>
 
 Todo:
 
- * Merge proper grammar.
- * Remove third argument from symbol tokens? (';', 1, ';')
  * Automate testing!
--- src/lexer/lexer_driver.erl
+++ src/lexer/lexer_driver.erl
@@ -2,33 +2,26 @@
 -export([start/0]).
 
 start() ->
-    %Tokens = process_file(standard_io),
-    String = getmamma(),
-    {ok, Tokens, _} = lexer:string(String),
+    String = stdin_to_string(),
+    Tokens = tokenize(String),
     term_io:term_to_stdout(Tokens),
     init:stop().
 
-getmamma() ->
+stdin_to_string() ->
     case io:get_chars('', 8192) of
         eof ->
             [];
         Text ->
-            Text ++ getmamma()
+            Text ++ stdin_to_string()
     end.
 
-process_file(Stream) ->
-    case next_token(Stream) of
-        {ok, Token, _} ->
-            [Token | process_file(Stream)];
-        {eof, _} ->
-            [];
-        ErrorInfo ->
-            die(ErrorInfo)
+tokenize(String) ->
+    case lexer:string(String) of
+        {ok, Tokens, _} ->
+            Tokens;
+        Error ->
+            die(Error)
     end.
-
-next_token(Stream) ->
-    Options = {get_until, '', lexer, token, [1]},
-    io:request(Stream, Options).
 
 die(Message) ->
     io:format('~p~n', [{lexical_error, Message}]),
