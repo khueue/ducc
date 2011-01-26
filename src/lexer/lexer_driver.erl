0665a09ec09ecf7156869f43dc4f07e49932ace3
Sebastian Lundström <khueue@gmail.com>
2011-01-26 16:40:48 +0100

    Line numbers by lexer.

--- src/lexer/lexer_driver.erl
+++ src/lexer/lexer_driver.erl
@@ -2,9 +2,19 @@
 -export([start/0]).
 
 start() ->
-    Tokens = process_file(standard_io),
+    %Tokens = process_file(standard_io),
+    String = getmamma(),
+    {ok, Tokens, _} = lexer:string(String),
     term_io:term_to_stdout(Tokens),
     init:stop().
+
+getmamma() ->
+    case io:get_chars('', 8192) of
+        eof ->
+            [];
+        Text ->
+            Text ++ getmamma()
+    end.
 
 process_file(Stream) ->
     case next_token(Stream) of
