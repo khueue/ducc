ec9260e0fd9c0b0d2b9bb0999768d22a8a2452e7
khueue <khueue@gmail.com>
2011-01-27 18:49:27 +0100

    Cleanup.

--- src/io_tools.erl
+++ src/io_tools.erl
@@ -1,6 +1,7 @@
 -module(io_tools).
 -export([
     string_from_input/0,
+    string_from_file/1,
     string_from_stream/1,
     term_from_string/1,
     term_to_output/1,
@@ -9,6 +10,12 @@
 
 string_from_input() ->
     string_from_stream(standard_io).
+
+string_from_file(File) ->
+    {ok, Stream} = file:open(File, [read]),
+    String = string_from_stream(Stream),
+    file:close(Stream),
+    String.
 
 string_from_stream(Stream) ->
     case io:get_chars(Stream, _Prompt = '', 8192) of
