3ff1600c449e1e81585d0bc2b14b5a0005f962cb
khueue <khueue@gmail.com>
2011-01-29 13:25:28 +0100

    Function call stuff.

--- testfil2.c
+++ testfil2.c
@@ -1,9 +1,18 @@
 char a;
 int b[42];
 void c(void);
-void d(int e[], int f, int e[], int f)
+
+void
+
+d
+
+(int e[], int f, int e[], int f)
+
 {
-    b(1, 2, 3+4);
+    a();
+    c(1);
+    d(1, 2);
+    b(1, 2, b = c);
     ;
     return e;
     return;
