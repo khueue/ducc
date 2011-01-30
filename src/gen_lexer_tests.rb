83c409aac4e728a588637dc7558f7c41ef0d279b
khueue <khueue@gmail.com>
2011-01-30 14:01:33 +0100

    Rewritten test generators. Now dynamic.

---
+++ src/gen_lexer_tests.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby -w
+# encoding: UTF-8
+
+# Run from project root: [ruby] src/gen_lexer_tests.rb
+
+if ARGV.empty?
+  puts "Usage: ruby #{__FILE__} glob_pattern [glob_pattern ...]"
+  exit
+end
+
+program = 'lexer'
+pipes = [program].join(' | ')
+
+files = []
+
+ARGV.each do |pattern|
+  files |= Dir[pattern]
+end
+
+files.each do |file|
+  file_out = "#{file}.#{program}"
+  cmd = "cat #{file} | #{pipes} > #{file_out}"
+  puts cmd
+  system cmd
+end
