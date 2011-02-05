3ee33e05f8bd50bbe20859b953a06574098120f6
ceh <c.emil.hessman@gmail.com>
2011-02-05 17:27:29 +0100

    Spring cleaning; helper functions goes to own module.

--- src/analyzer/analyzer.erl
+++ src/analyzer/analyzer.erl
@@ -1,23 +1,7 @@
 -module(analyzer).
 -export([analyze/1]).
 
-get_tag(Node) ->
-    {_Line, Tag} = get_meta(Node),
-    Tag.
-
-get_line(Node) ->
-    {Line, _Tag} = get_meta(Node),
-    Line.
-
-get_type(Node) ->
-    erlang:element(2, Node).
-
-get_meta(Node) ->
-    erlang:element(1, Node).
-
-exception(Node, Format, Args) ->
-    Message = io_lib:format(Format, Args),
-    {analyzer_exception, {get_line(Node), Message}}.
+-define(HELPER, analyzer_helpers).
 
 analyze(ParseTree) ->
     Env = analyzer_env:new(),
@@ -25,7 +9,7 @@ analyze(ParseTree) ->
     ParseTree.
 
 analyze(Node, Env0) when erlang:is_tuple(Node) ->
-    Tag = get_tag(Node),
+    Tag = ?HELPER:get_tag(Node),
     Env1 = analyze_node(Tag, Node, Env0),
     Env1;
 analyze(nil, Env0) ->
@@ -68,7 +52,7 @@ must_not_exist_in_same_scope(Name, Node, Env) ->
         not_found ->
             ok;
         _SymbolInfo ->
-            throw(exception(Node, 'already defined', []))
+            throw(?HELPER:exception(Node, 'already defined', []))
     end.
 
 analyze_arraydec(Node = {_Meta, _Type, Name, _Size}, Env0) ->
@@ -78,12 +62,12 @@ analyze_arraydec(Node = {_Meta, _Type, Name, _Size}, Env0) ->
     Env1.
 
 analyze_fundec(Node = {_Meta, _Type, Name, Formals}, Env0) ->
-    Redefinition = exception(Node, 'already defined', []),
+    Redefinition = ?HELPER:exception(Node, 'already defined', []),
     Env1 = case analyzer_env:lookup(Name, Node, Env0) of
         not_found ->
             analyzer_env:add_symbol(Name, Node, Env0);
         FoundNode ->
-            case get_tag(FoundNode) of
+            case ?HELPER:get_tag(FoundNode) of
                 fundec ->
                     same_return_type(Node, FoundNode),
                     same_formals(Node, FoundNode),
@@ -101,12 +85,12 @@ analyze_fundec(Node = {_Meta, _Type, Name, Formals}, Env0) ->
     Env1. % Updates to the environment are local to the function!
 
 analyze_fundef(Node = {_Meta, _Type, Name, Formals, Locals, Stmts}, Env0) ->
-    Redefinition = exception(Node, 'already defined', []),
+    Redefinition = ?HELPER:exception(Node, 'already defined', []),
     Env1 = case analyzer_env:lookup(Name, Node, Env0) of
         not_found ->
             analyzer_env:add_symbol(Name, Node, Env0);
         FoundNode ->
-            case get_tag(FoundNode) of
+            case ?HELPER:get_tag(FoundNode) of
                 fundec ->
                     same_return_type(Node, FoundNode),
                     same_formals(Node, FoundNode),
@@ -128,18 +112,18 @@ same_formals(Node, FoundNode) ->
     identical_types(NodeFormals, FoundNodeFormals, Node).
 
 same_return_type(Node, FoundNode) ->
-    case get_type(Node) =:= get_type(FoundNode) of
+    case ?HELPER:get_type(Node) =:= ?HELPER:get_type(FoundNode) of
         true ->
             ok;
         false ->
-            throw(exception(Node, 'wrong return type', []))
+            throw(?HELPER:exception(Node, 'wrong return type', []))
     end.
 
 same_arity(Formals, FoundFormals, Node) ->
     try same_arity(Formals, FoundFormals)
     catch
         different_arity ->
-            throw(exception(Node, 'wrong number of arguments', []))
+            throw(?HELPER:exception(Node, 'wrong number of arguments', []))
     end.
 
 same_arity(Formals, FoundFormals) ->
@@ -160,7 +144,7 @@ identical_types([F1|Formals1], [F2|Formals2], CurrentNode) ->
 same_tag_and_type({{_,Tag},Type,_}, {{_,Tag},Type,_}, _CurrentNode) ->
     ok;
 same_tag_and_type(_, _, CurrentNode) ->
-    throw(exception(CurrentNode, 'parameters must match exactly', [])).
+    throw(?HELPER:exception(CurrentNode, 'parameters must match exactly', [])).
 
 analyze_formal_arraydec(Node = {_Meta, _Type, Name}, Env0) ->
     must_not_exist_in_same_scope(Name, Node, Env0),
@@ -171,20 +155,22 @@ analyze_if(Node = {_Meta, Cond, Then, Else}, Env) ->
     Env1 = analyze(Cond, Env),
     Env2 = analyze(Then, Env1),
     Env3 = analyze(Else, Env2),
-    convertible_to({if_cond, int}, eval_type(Cond, Env3), Node),
+    ?HELPER:convertible_to({if_cond, int}, ?HELPER:eval_type(Cond, Env3), Node),
     Env3.
 
 analyze_while(Node = {_Meta, Cond, Stmt}, Env) ->
     Env1 = analyze(Cond, Env),
     Env2 = analyze(Stmt, Env1),
-    convertible_to({while_cond, int}, eval_type(Cond, Env2), Node),
+    ?HELPER:convertible_to({while_cond, int}, ?HELPER:eval_type(Cond, Env2),
+                           Node),
     Env2.
 
 analyze_return(Node = {_Meta, Expr}, Env) ->
     Env1 = analyze(Expr, Env),
     FunName = analyzer_env:scope_name(Env1),
     FunNode = analyzer_env:lookup(FunName, Node, Env1),
-    convertible_to(eval_type(FunNode, Env1), eval_type(Expr, Env1), Node),
+    ?HELPER:convertible_to(?HELPER:eval_type(FunNode, Env1),
+                           ?HELPER:eval_type(Expr, Env1), Node),
     Env1.
 
 analyze_funcall(Node = {_Meta, Name, Actuals}, Env0) ->
@@ -198,28 +184,23 @@ check_actuals(Function, Funcall, Env) ->
     Formals = erlang:element(4, Function),
     Actuals = erlang:element(3, Funcall),
     same_arity(Formals, Actuals, Funcall),
-    convertible_types(Formals, Actuals, Env).
-
-convertible_types([], [], _Env) -> ok;
-convertible_types([F|Formals], [A|Actuals], Env) ->
-    convertible_to(eval_type(F, Env), eval_type(A, Env), A),
-    convertible_types(Formals, Actuals, Env).
+    ?HELPER:convertible_types(Formals, Actuals, Env).
 
 analyze_arrelem(Node = {_Meta, Name, Index}, Env) ->
     must_be_tag(Name, Node, Env, [arraydec,formal_arraydec]),
     Env1 = analyze(Index, Env),
-    convertible_to({dontcare,int}, eval_type(Index, Env1), Node),
+    ?HELPER:convertible_to({dontcare,int}, ?HELPER:eval_type(Index, Env1), Node),
     Env1.
 
 must_be_tag(Name, Node, Env, Tags) ->
     case analyzer_env:lookup(Name, Node, Env) of
         not_found ->
-            throw(exception(Node, 'not defined', []));
+            throw(?HELPER:exception(Node, 'not defined', []));
         FoundNode ->
-            Tag = get_tag(FoundNode),
+            Tag = ?HELPER:get_tag(FoundNode),
             case lists:member(Tag, Tags) of
                 true  -> ok;
-                false -> throw(exception(Node, 'not proper type', []))
+                false -> throw(?HELPER:exception(Node, 'not proper type', []))
             end
     end.
 
@@ -229,21 +210,22 @@ analyze_binop(Node = {_Meta, Lhs, Op, Rhs}, Env0) ->
     case Op of
         '=' ->
             must_be_lval(Lhs, Env2),
-            convertible_to(eval_type(Lhs, Env2), eval_type(Rhs, Env2), Node);
+            ?HELPER:convertible_to(?HELPER:eval_type(Lhs, Env2),
+                                   ?HELPER:eval_type(Rhs, Env2), Node);
         _Other ->
-            eval_type(Node, Env2)
+            ?HELPER:eval_type(Node, Env2)
     end,
     Env2.
 
 must_be_lval(Node = {{_,ident},Name}, Env) ->
     FoundNode = analyzer_env:lookup(Name, Node, Env),
-    case get_tag(FoundNode) of
+    case ?HELPER:get_tag(FoundNode) of
         scalardec -> ok;
-        _Other    -> throw(exception(Node, 'not an l-value', []))
+        _Other    -> throw(?HELPER:exception(Node, 'not an l-value', []))
     end;
 must_be_lval({{_,arrelem},_Name,_Index}, _Env) -> ok;
 must_be_lval(Node, _Env) ->
-    throw(exception(Node, 'not an l-value', [])).
+    throw(?HELPER:exception(Node, 'not an l-value', [])).
 
 analyze_ident(Node = {_Meta, Name}, Env) ->
     must_be_defined(Name, Node, Env),
@@ -252,7 +234,7 @@ analyze_ident(Node = {_Meta, Name}, Env) ->
 must_be_defined(Name, Node, Env) ->
     case analyzer_env:lookup(Name, Node, Env) of
         not_found ->
-            throw(exception(Node, 'not defined', []));
+            throw(?HELPER:exception(Node, 'not defined', []));
         _FoundNode ->
             ok
     end.
@@ -265,74 +247,5 @@ analyze_charconst({_Meta, _Char}, Env) ->
 
 analyze_unop(Node = {_Meta, _Op, Rhs}, Env) ->
     Env1 = analyze(Rhs, Env),
-    convertible_to({dontcare,int}, eval_type(Rhs, Env1), Node),
+    ?HELPER:convertible_to({dontcare,int}, ?HELPER:eval_type(Rhs, Env1), Node),
     Env1.
-
-% make sure all exprs are covered xxxx
-eval_type(nil, _Env) ->
-    {return_expr, void};
-eval_type(Node = {{_, binop}, Lhs, _Op, Rhs}, Env) ->
-    Type = widest_type(eval_type(Lhs, Env), eval_type(Rhs, Env), Node),
-    {binop, Type};
-eval_type(_Node = {{_, unop}, _Op, Rhs}, Env) ->
-    {_, Type} = eval_type(Rhs, Env),
-    {unop, Type};
-eval_type(Node = {{_, ident}, Name}, Env) ->
-    FoundNode = analyzer_env:lookup(Name, Node, Env),
-    {get_tag(FoundNode), get_type(FoundNode)};
-eval_type(_Node = {{_, intconst}, _Name}, _Env) ->
-    {intconst, int};
-eval_type(_Node = {{_, charconst}, _Name}, _Env) ->
-    {charconst, char};
-eval_type(Node = {{_, arrelem}, Name, _Index}, Env) ->
-    FoundNode = analyzer_env:lookup(Name, Node, Env),
-    {arrelem, get_type(FoundNode)};
-eval_type(Node = {{_, funcall}, Name, _Actuals}, Env) ->
-    FoundNode = analyzer_env:lookup(Name, Node, Env),
-    {funcall, get_type(FoundNode)};
-eval_type(_Node = {{_, fundec}, Type, _Name, _Formals}, _Env) ->
-    {fundec, Type};
-eval_type(_Node = {{_, fundef}, Type, _Name, _Formals, _Locals, _Stmts}, _Env) ->
-    {fundef, Type};
-eval_type(_Node = {{_, scalardec}, Type, _Name}, _Env) ->
-    {scalardec, Type};
-eval_type(_Node = {{_, arraydec}, Type, _Name, _Size}, _Env) ->
-    {arraydec, Type};
-eval_type(_Node = {{_, formal_arraydec}, Type, _Name}, _Env) ->
-    {formal_arraydec, Type}.
-
-widest_type(Type1, Type2, Node) ->
-    try widest_type(Type1, Type2)
-    catch
-        incompatible ->
-            throw(exception(Node, 'incompatible types', []))
-    end.
-
-widest_type({_,_}, {arraydec,_})        -> throw(incompatible);
-widest_type({arraydec,_}, {_,_})        -> throw(incompatible);
-widest_type({_,_}, {formal_arraydec,_}) -> throw(incompatible);
-widest_type({formal_arraydec,_}, {_,_}) -> throw(incompatible);
-widest_type({_,int}, {_,int})           -> int;
-widest_type({_,int}, {_,char})          -> int;
-widest_type({_,char}, {_,char})         -> char;
-widest_type({_,char}, {_,int})          -> int;
-widest_type({_,_}, {_,_})               -> throw(incompatible).
-
-convertible_to(ExpectedTuple, ActualTuple, Actual) ->
-    try first_accepts_second(ExpectedTuple, ActualTuple)
-    catch
-        incompatible ->
-            throw(exception(Actual, 'inconvertible types', []))
-    end.
-
-first_accepts_second({formal_arraydec, Type}, {arraydec, Type}) -> ok;
-first_accepts_second({formal_arraydec, Type}, {formal_arraydec, Type}) -> ok;
-first_accepts_second(_, {arraydec,_})    -> throw(incompatible);
-first_accepts_second({arraydec,_}, _)    -> throw(incompatible);
-first_accepts_second(_, {formal_arraydec,_})    -> throw(incompatible);
-first_accepts_second({_,void}, {_,void}) -> ok;
-first_accepts_second({_,int}, {_,int})   -> ok;
-first_accepts_second({_,int}, {_,char})  -> ok;
-first_accepts_second({_,char}, {_,char}) -> ok;
-first_accepts_second({_,char}, {_,int})  -> ok;
-first_accepts_second(_, _)               -> throw(incompatible).
---
+++ src/analyzer/analyzer_helpers.erl
@@ -0,0 +1,100 @@
+-module(analyzer_helpers).
+-export([exception/3,
+         get_meta/1,
+         get_tag/1,
+         get_type/1,
+         convertible_types/3,
+         convertible_to/3,
+         eval_type/2
+         ]).
+
+exception(Node, Format, Args) ->
+    Message = io_lib:format(Format, Args),
+    {analyzer_exception, {get_line(Node), Message}}.
+
+get_line(Node) ->
+    {Line, _Tag} = get_meta(Node),
+    Line.
+
+get_meta(Node) ->
+    erlang:element(1, Node).
+
+get_tag(Node) ->
+    {_Line, Tag} = get_meta(Node),
+    Tag.
+
+get_type(Node) ->
+    erlang:element(2, Node).
+
+convertible_types([], [], _Env) -> ok;
+convertible_types([F|Formals], [A|Actuals], Env) ->
+    convertible_to(eval_type(F, Env), eval_type(A, Env), A),
+    convertible_types(Formals, Actuals, Env).
+
+convertible_to(ExpectedTuple, ActualTuple, Actual) ->
+    try first_accepts_second(ExpectedTuple, ActualTuple)
+    catch
+        incompatible ->
+            throw(exception(Actual, 'inconvertible types', []))
+    end.
+
+eval_type(nil, _Env) ->
+    {return_expr, void};
+eval_type(Node = {{_, binop}, Lhs, _Op, Rhs}, Env) ->
+    Type = widest_type(eval_type(Lhs, Env), eval_type(Rhs, Env), Node),
+    {binop, Type};
+eval_type(_Node = {{_, unop}, _Op, Rhs}, Env) ->
+    {_, Type} = eval_type(Rhs, Env),
+    {unop, Type};
+eval_type(Node = {{_, ident}, Name}, Env) ->
+    FoundNode = analyzer_env:lookup(Name, Node, Env),
+    {get_tag(FoundNode), get_type(FoundNode)};
+eval_type(_Node = {{_, intconst}, _Name}, _Env) ->
+    {intconst, int};
+eval_type(_Node = {{_, charconst}, _Name}, _Env) ->
+    {charconst, char};
+eval_type(Node = {{_, arrelem}, Name, _Index}, Env) ->
+    FoundNode = analyzer_env:lookup(Name, Node, Env),
+    {arrelem, get_type(FoundNode)};
+eval_type(Node = {{_, funcall}, Name, _Actuals}, Env) ->
+    FoundNode = analyzer_env:lookup(Name, Node, Env),
+    {funcall, get_type(FoundNode)};
+eval_type(_Node = {{_, fundec}, Type, _Name, _Formals}, _Env) ->
+    {fundec, Type};
+eval_type(_Node = {{_, fundef}, Type, _Name, _Formals, _Locals, _Stmts}, _Env) ->
+    {fundef, Type};
+eval_type(_Node = {{_, scalardec}, Type, _Name}, _Env) ->
+    {scalardec, Type};
+eval_type(_Node = {{_, arraydec}, Type, _Name, _Size}, _Env) ->
+    {arraydec, Type};
+eval_type(_Node = {{_, formal_arraydec}, Type, _Name}, _Env) ->
+    {formal_arraydec, Type}.
+
+widest_type(Type1, Type2, Node) ->
+    try widest_type(Type1, Type2)
+    catch
+        incompatible ->
+            throw(exception(Node, 'incompatible types', []))
+    end.
+
+widest_type({_,_}, {arraydec,_})        -> throw(incompatible);
+widest_type({arraydec,_}, {_,_})        -> throw(incompatible);
+widest_type({_,_}, {formal_arraydec,_}) -> throw(incompatible);
+widest_type({formal_arraydec,_}, {_,_}) -> throw(incompatible);
+widest_type({_,int}, {_,int})           -> int;
+widest_type({_,int}, {_,char})          -> int;
+widest_type({_,char}, {_,char})         -> char;
+widest_type({_,char}, {_,int})          -> int;
+widest_type({_,_}, {_,_})               -> throw(incompatible).
+
+first_accepts_second({formal_arraydec, Type}, {arraydec, Type})        -> ok;
+first_accepts_second({formal_arraydec, Type}, {formal_arraydec, Type}) -> ok;
+first_accepts_second(_, {arraydec,_}) -> throw(incompatible);
+first_accepts_second({arraydec,_}, _) -> throw(incompatible);
+first_accepts_second(_, {formal_arraydec,_}) -> throw(incompatible);
+first_accepts_second({_,void}, {_,void}) -> ok;
+first_accepts_second({_,int}, {_,int})   -> ok;
+first_accepts_second({_,int}, {_,char})  -> ok;
+first_accepts_second({_,char}, {_,char}) -> ok;
+first_accepts_second({_,char}, {_,int})  -> ok;
+first_accepts_second(_, _) -> throw(incompatible).
