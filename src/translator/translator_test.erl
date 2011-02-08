25793b77bb1f982a65bef49bbbfc3a092d65fadb
ceh <c.emil.hessman@gmail.com>
2011-02-08 18:19:27 +0100

    Add translator stubs.

---
+++ src/translator/translator_test.erl
@@ -0,0 +1,11 @@
+-module(translator_test).
+-include_lib("eunit/include/eunit.hrl").
+
+translator_test_() ->
+    Tests = all_tests(),
+    Program = "translator",
+    Fun = fun() -> test_tools:run_all_tests(Tests, Program) end,
+    test_tools:test_tuple(Fun).
+
+all_tests() ->
+    filelib:fold_files("suite", ".+.c$", true, fun(F, Acc) -> [F|Acc] end, []).
