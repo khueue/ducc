52eaa3fca19c4293b48dee32c2612c01c40f0d33
ceh <c.emil.hessman@gmail.com>
2011-02-09 12:32:26 +0100

    Clean.

--- src/translator/translator_driver.erl
+++ src/translator/translator_driver.erl
@@ -2,15 +2,13 @@
 -export([translate/2]).
 
 translate(Stream, ParseTree) ->
-    Instr = try translator:translate(ParseTree)
+    Instructions = try translator:translate(ParseTree)
     catch
         {translator_exception, {Line, Message}} ->
             Message1 = format_error(Stream, Line, Message),
             throw({translator_exception, Message1})
     end,
-    {ok, Instr}.
+    {ok, Instructions}.
 
 format_error(Stream, Line, Message) ->
-    io_lib:format(
-        '~s:~p: translation error, ~s~n',
-        [Stream, Line, Message]).
+    io_lib:format('~s:~p: translation error, ~s~n', [Stream, Line, Message]).
