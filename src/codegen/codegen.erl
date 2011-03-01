d7631c1adf23872cea10b7a5c520acf5e5415a8b
Sebastian Lundström <khueue@gmail.com>
2011-03-01 10:19:40 +0100

    Structure.

--- src/codegen/codegen.erl
+++ src/codegen/codegen.erl
@@ -6,8 +6,23 @@
 
 generate_code(RtlCode, Lines) ->
     Env = ?ENV:new(Lines),
-    AsmCode = generate(RtlCode, Env),
+    AsmCode = translate_toplevels(RtlCode, Env),
     AsmCode.
 
-generate(X, _Env) ->
-    X.
+translate_toplevels([], _Env) -> [];
+translate_toplevels([Toplevel|Toplevels], Env) ->
+    ToplevelAsm = translate_toplevel(Toplevel, Env),
+    [ToplevelAsm|translate_toplevels(Toplevels, Env)].
+
+translate_toplevel(Toplevel, Env) ->
+    Type = erlang:element(1, Toplevel),
+    case Type of
+        data -> translate_data(Toplevel, Env);
+        proc -> translate_proc(Toplevel, Env)
+    end.
+
+translate_data({data, {label,Name}, Bytes}, _Env) ->
+    data.
+
+translate_proc({proc, {label,Name}, Formals, Temps, FS, Ins}, _Env) ->
+    proc.
