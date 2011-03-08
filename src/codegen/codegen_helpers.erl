8d9d51aadc64cd336d20d4d6270c96f153272a39
ceh <c.emil.hessman@gmail.com>
2011-03-08 17:58:46 +0100

    Clean cjump.

--- src/codegen/codegen_helpers.erl
+++ src/codegen/codegen_helpers.erl
@@ -1,22 +1,26 @@
 -module(codegen_helpers).
 -export([
-    asm_fun/1,
+    asm_binop_fun/1,
+    asm_cjump_fun/1,
     calculate_frame_size/2,
     setup_function_prologue/2,
     setup_function_epilogue/3]).
 
 -define(ASM, codegen_asm).
 
-asm_fun('+')  -> fun ?ASM:asm_add/3;
-asm_fun('-')  -> fun ?ASM:asm_sub/3;
-asm_fun('*')  -> fun ?ASM:asm_mul/3;
-asm_fun('/')  -> fun ?ASM:asm_div/3;
-asm_fun('<')  -> fun ?ASM:asm_slt/3;
-asm_fun('<=') -> fun ?ASM:asm_sle/3;
-asm_fun('>')  -> fun ?ASM:asm_sgt/3;
-asm_fun('>=') -> fun ?ASM:asm_sge/3;
-asm_fun('==') -> fun ?ASM:asm_seq/3;
-asm_fun('!=') -> fun ?ASM:asm_sne/3.
+asm_binop_fun('+')  -> fun ?ASM:asm_add/3;
+asm_binop_fun('-')  -> fun ?ASM:asm_sub/3;
+asm_binop_fun('*')  -> fun ?ASM:asm_mul/3;
+asm_binop_fun('/')  -> fun ?ASM:asm_div/3;
+asm_binop_fun('<')  -> fun ?ASM:asm_slt/3;
+asm_binop_fun('<=') -> fun ?ASM:asm_sle/3;
+asm_binop_fun('>')  -> fun ?ASM:asm_sgt/3;
+asm_binop_fun('>=') -> fun ?ASM:asm_sge/3;
+asm_binop_fun('==') -> fun ?ASM:asm_seq/3;
+asm_binop_fun('!=') -> fun ?ASM:asm_sne/3.
+
+asm_cjump_fun(eq)  -> fun ?ASM:asm_beqz/2;
+asm_cjump_fun(neq) -> fun ?ASM:asm_bnez/2.
 
 calculate_frame_size(Locals, ArraysSize) ->
     4 + 4 + % FP and RA.
